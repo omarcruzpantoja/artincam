6bc800ad4465ec0c15bd63ef2a201d3f8e534933
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-03-19 03:50:18 -0400

    refactor(camera): replace camera stream and add camera pictures

--- camera/artincam/camera.py
+++ camera/artincam/camera.py
@@ -1,24 +1,24 @@
 import json
 import time
-import os
 import pathlib
+
 # import setproctitle
 from enum import StrEnum
 from datetime import datetime
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput, FfmpegOutput
-
+from picamera2.outputs import FileOutput
 
 ROOT_DIRECTORY = pathlib.Path(__file__).resolve().parent
-PID_STORE = "/tmp/articam.txt" #  TODO: make this shareable between scripts
+
 
 class TimeUnit(StrEnum):
     SECOND = "s"
     MINUTE = "m"
     HOUR = "h"
     DAY = "d"
+
 
 class Camera:
     _config: dict
@@ -42,42 +42,35 @@ class Camera:
         self.picam = Picamera2()
         self._validate_and_set_config(ROOT_DIRECTORY / config_path)
         pass
-        
+
     def setup(self):
         video_config = self.picam.create_video_configuration(
             main={"size": (self._height, self._width)},
-            controls={'FrameRate': self._framerate}
+            controls={"FrameRate": self._framerate},
         )
         self.picam.configure(video_config)
         self.encoder = H264Encoder(bitrate=self._bitrate, framerate=self._framerate)
 
         self.file_output = FileOutput()
-        self.rtsp_stream_output = FfmpegOutput(
-            "-f rtsp -rtsp_transport udp rtsp://omarcam:123cam@localhost:8554/camstream",
-            audio=False
-        )
-
-        self.encoder.output = [self.file_output, self.rtsp_stream_output]
+        self.encoder.output = [self.file_output]
 
     def run(self):
-        
-
         # loop forever
-        self.picam.start_encoder(self.encoder)
-        self.picam.start()
+        # self.picam.start_encoder(self.encoder)
+        # self.picam.start()
         try:
-            while(True):
+            while True:
                 output_file = self._get_file_name()
                 self.file_output.fileoutput = output_file
                 # start video
-                self.file_output.start()
+                # self.file_output.start()
                 print(f"Recording started ({self._recording_time}s)")
                 # sleep is needed for it to continue recording
                 time.sleep(self._recording_time)
 
                 # once time is finished, stop recording
                 print(f"Recording Finished and stored ({output_file}")
-                self.file_output.stop()
+                # self.file_output.stop()
 
                 print(f"Resting...({self._rest_time})")
                 time.sleep(self._rest_time)
@@ -86,62 +79,74 @@ class Camera:
             self.picam.stop()
 
     def _validate_and_set_config(self, path: str):
+        # TODO: add error handling if file doesnt exist
         self._config = json.loads(open(path, "r").read())
-        camera_config = self._config["camera"]
+        camera_config: dict = self._config["camera"]
 
-        # TODO: add code to validate here
-        self._set_time_unit_conversion(camera_config["unit_time"])
+        # unit used to define video recording time, default is minutes (m)
+        self._set_time_unit_conversion(camera_config.get("unit_time", TimeUnit.MINUTE))
 
-        # TODO: validation, recording time cannot be less than 5 seconds
-        self._recording_time = camera_config["recording_time"] * self._mutiplier
-        
-        self._framerate = camera_config["framerate"]
+        # how long should each video be, default is 10
+        self._recording_time = camera_config.get("recording_time", 10) * self._mutiplier
 
-        # TODO: validation, recording time cannot be less than 5 seconds
-        self._rest_time = camera_config["rest_time"] * self._mutiplier
-        self._bitrate = camera_config["bitrate"]
-        self._width = camera_config["resolution"]["width"]
-        self._height = camera_config["resolution"]["height"]
+        # how many frames per second, default 24
+        self._framerate = camera_config.get("framerate", 24)
+
+        # rest time defines how much time to wait till next video starts being saved, default is 0
+        # this means, once a video is finished being recorded, the next one will start being recorded
+        # immediately
+        self._rest_time = camera_config.get("rest_time", 0) * self._mutiplier
+
+        # bitrate is used to determine the quality of image during compression. The higher the value the
+        # better quality image, the more space it takes. Input is expected to be provided as MB. Meaning
+        # if you want 16MB birate, bitrate value should be 16. Default is 8MB
+        self._bitrate = camera_config.get("bitrate", 8) * 2**20
+
+        # how many width pixels, think of it as if you were providing measurements for a box ( width x height )
+        # the bigger width x height, the better the image, the more storage it takes. Default is 1920
+        self._width = camera_config.get("resolution", {}).get("width", 1920)
+        # how many height pixels, default is 1080
+        self._height = camera_config.get("resolution", {}).get("height", 1080)
+
+        # location of where the rapsberry pi will be located in, not default value
         self._location = camera_config["location"]
+        # identifier of raspberry pi, no default value
         self._pi_id = camera_config["pi_id"]
 
+        # where to store recordings
         # TODO: if directory does not exist, create it
         self._output_dir = camera_config["output_dir"]
 
     def _set_time_unit_conversion(self, unit: TimeUnit):
+        """Depending on the file config's time unit, define the multiplier to convert whatever unit
+        provided to seconds.
+        """
         match unit:
             case TimeUnit.SECOND:
                 self._mutiplier = 1
                 # do nothing
                 pass
             case TimeUnit.MINUTE:
+                # Theres 60 seconds in a method
                 self._mutiplier = 60
                 pass
             case TimeUnit.HOUR:
+                # in 60 minutes in an hour, each minute has 60 seconds = 60 * 60
                 self._mutiplier = 3600
                 pass
             case TimeUnit.DAY:
-                self._mutiplier = 86300
+                # 24 hours a day, 60 minutes in an hour, each minute has 60 seconds = 24 * 60 * 60
+                self._mutiplier = 86400
 
-    def _get_file_name(self) -> pathlib.Path:
+    def _get_file_name(self) -> str:
+        """Defines the name of the file generated for the video."""
+
         # the timestamp format here aims to do: dd_mm_yyyy_hh_mm
         # Example: Say its Feb 20 2025, 6:03:10AM. The format would look like: 20_02_2025_06_03_10
         timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
 
         # The complete filename however would look like:
-        # 1_sj_pr_usa_25_02_2025_01_00_10.h264
+        # 1_sj_pr_usa_20_02_2025_06_03_10.h264
         # Assuming _pi_id = 1, _location = sj_pr_usa
-        return str(ROOT_DIRECTORY / self._output_dir / f"{self._pi_id}_{self._location}_{timestamp}.h264")
-
-def add_process_id():
-    with open(PID_STORE, 'w') as f:
-        f.write(str(os.getpid()))
-
-        f.close()
-
-if __name__ == "__main__":
-    # setproctitle.setproctitle('articam-py')
-    add_process_id()
-    camera = Camera()
-    camera.setup()
-    camera.run()
+        file_name = f"{self._pi_id}_{self._location}_{timestamp}.h264"
+        return f"{ROOT_DIRECTORY}{self._output_dir}{file_name}"
---
+++ camera/artincam/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from camera!")
+
+
+if __name__ == "__main__":
+    main()
