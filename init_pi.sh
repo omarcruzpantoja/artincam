1c3f549da8385e91fb1200720d85c41d8a5e39d8
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-04-02 06:35:18 -0400

    feature(camera): add artincam camera service config

--- init_pi.sh
+++ init_pi.sh
@@ -3,3 +3,22 @@
 # allow ssh service to turn on automatically on boot
 sudo touch /boot/ssh
 
+# Install UV
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Go into camera folder to set up camera script
+cd camera
+
+# Create virtual environment, use system site packages to take advantage of already installed
+# picamera libraries and packages
+uv venv --system-site-packages
+
+# Install "project" dependencies
+uv sync --all-extras
+
+sudo cp artincam/setup/articam.service /etc/systemd/system/artincam.service
+
+sudo mv ~/Documents/artincam /opt/artincam
+sudo chown -R $USER:$USER /opt/artincam
+
+sudo systemctl daemon-reload
