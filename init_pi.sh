c724838df832100b67b9f60db95fa066fb95ce4f
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-04-07 09:03:44 -0400

    upload latest init pi

--- init_pi.sh
+++ init_pi.sh
@@ -6,6 +6,9 @@ sudo touch /boot/ssh
 # Install UV
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# Restart bash so uv is installed
+source ~/.bashrc
+
 # Go into camera folder to set up camera script
 cd camera
 
@@ -16,9 +19,14 @@ uv venv --system-site-packages
 # Install "project" dependencies
 uv sync --all-extras
 
-sudo cp artincam/setup/articam.service /etc/systemd/system/artincam.service
+sudo cp setup/artincam.service /etc/systemd/system/artincam.service
 
-sudo mv ~/Documents/artincam /opt/artincam
+sudo mv ~/artincam /opt/artincam
 sudo chown -R $USER:$USER /opt/artincam
 
 sudo systemctl daemon-reload
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
