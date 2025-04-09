ae06529ed4d8cbd47de4837288b67c2c3f13764e
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-04-08 20:47:23 -0400

    add bluetooth setup

--- init_pi.sh
+++ init_pi.sh
@@ -3,6 +3,9 @@
 # allow ssh service to turn on automatically on boot
 sudo touch /boot/ssh
 
+# Get current user
+CURRENT_USER=$(whoami)
+
 # Install UV
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
@@ -19,10 +22,20 @@ uv venv --system-site-packages
 # Install "project" dependencies
 uv sync --all-extras
 
-sudo cp setup/artincam.service /etc/systemd/system/artincam.service
+# Path to the service file
+SERVICE_FILE="setup/artincam.service"
+
+# Replace the User field in the service file
+sed -i "s/^User=.*/User=$CURRENT_USER/" "$SERVICE_FILE"
+
+sudo cp $SERVICE_FILE /etc/systemd/system/artincam.service
 
 sudo mv ~/artincam /opt/artincam
 sudo chown -R $USER:$USER /opt/artincam
+
+# ---- Set up Blueooth ----
+cd ../
+bash ./bluetooth_setup.sh
 
 sudo systemctl daemon-reload
 
