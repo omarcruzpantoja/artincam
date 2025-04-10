561a5000152021d1f8fff8ba728588dd276020b2
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-04-09 20:16:02 -0400

    update bluetooth setup

--- bluetooth_setup.sh
+++ bluetooth_setup.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # Prompt the user for a Bluetooth name identifier for the Raspberry Pi
-echo "Provide Bluetooth name identifier for the Pi (e.g., 'zacpi-1'):"
+echo "Provide Bluetooth name identifier for the Pi (e.g., '1'):"
 read -p "Bluetooth Name Identifier: " PRETTY_HOSTNAME
 
 # Default to 'raspberrypi' if the user leaves it empty
 if [[ -z "$PRETTY_HOSTNAME" ]]; then
-  PRETTY_HOSTNAME="raspberrypi"
+  PRETTY_HOSTNAME="zacpi"
   echo "No input detected. Using default hostname: $PRETTY_HOSTNAME"
 fi
-
-# Update the machine-info file with the user-provided hostname
-echo "Setting machine-info with Bluetooth name: $PRETTY_HOSTNAME"
-echo "PRETTY_HOSTNAME=$PRETTY_HOSTNAME" | sudo tee /etc/machine-info >/dev/null
 
 # Edit /lib/systemd/system/bluetooth.service to enable Bluetooth services
 echo "Configuring Bluetooth service..."
 sudo sed -i 's|^ExecStart=.*toothd$| \
-ExecStart=/usr/lib/bluetooth/bluetoothd -C \
+ExecStart=/usr/libexec/bluetooth/bluetoothd -C \
 ExecStartPost=/usr/bin/sdptool add SP \
 ExecStartPost=/bin/hciconfig hci0 piscan \
 |g' /lib/systemd/system/bluetooth.service
@@ -31,11 +27,21 @@ After=bluetooth.service
 Requires=bluetooth.service
 
 [Service]
-ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a pi
+ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 xterm -a zacpi
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# Default to 'zacpi' if the user leaves it empty
+if [[ -z "$PRETTY_HOSTNAME" ]]; then
+  PRETTY_HOSTNAME="zacpi"
+  echo "No input detected. Using default hostname: $PRETTY_HOSTNAME"
+fi
+
+# Update the machine-info file with the user-provided hostname
+echo "Setting Bluetooth alias with: $PRETTY_HOSTNAME"
+sudo busctl set-property org.bluez /org/bluez/hci0 org.bluez.Adapter1 Alias s "zacpi-$PRETTY_HOSTNAME"
 
 # Reload systemd to apply changes
 echo "Reloading systemd to apply changes..."
