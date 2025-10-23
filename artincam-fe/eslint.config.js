9f6339f0176751e3a0c60352cefe6c5756c28b45
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-10-22 20:02:44 -0400

    add frontend init

---
+++ artincam-fe/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+      // Enable lint rules for React
+      reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
---
+++ artincam-fe/src/App.tsx
@@ -0,0 +1,12 @@
+import { type JSX } from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+}
+
+export default App;
